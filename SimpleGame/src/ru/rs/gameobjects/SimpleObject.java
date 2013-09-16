602f2121e07ff1aa5d45b22113edbcd20de75252
Suslov Vadik <rs.vsuslov@gmail.com>
2013-09-17 01:31:40 +0600

    добавил лаунчи для домашнего мака поправил немного simpleobject ввел метод получения позиции для рендера и отрисовку границ

--- SimpleGame/src/ru/rs/gameobjects/SimpleObject.java
+++ SimpleGame/src/ru/rs/gameobjects/SimpleObject.java
@@ -1,56 +1,73 @@
+
 package ru.rs.gameobjects;
 
 import ru.rs.GameObject;
 import ru.rs.Renderable;
 import ru.rs.interfaces.Game;
-import ru.rs.objects.math.Rectangle;
 import ru.rs.objects.math.Vector;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 
 public abstract class SimpleObject extends GameObject implements Renderable {
-	public Bitmap image;
-	public Side side;
-	public Game game;
-
-	public SimpleObject(float x, float y, float width, float height) {
-		super(x, y, width, height);
-	}
-
-	public SimpleObject(Vector position, Bitmap image) {
-		super(position.x, position.y, image.getWidth(), image.getHeight());
-		this.image = image;
-	}
-
-	public SimpleObject(Side side, Game game) {
-		this.side = side;
-		this.game = game;
-		setImage();
-
-		float x = (Side.ENEMY.equals(this.side)) ? this.game.getGraphics()
-				.getWidth() - image.getWidth() - 1 : 0;
-		float y = 1;
-
-		position = new Vector(x, y);
-		float w = image.getWidth();
-		float h = image.getHeight();
-		this.width = w;
-		this.height = h;
-		bounds = new Rectangle(x - w / 2, y - h / 2, w, h);
-	}
-
-	@Override
-	public void render() {
-		game.getGraphics()
-				.drawBitmap(
-						image,
-						position.x,
-						game.getGraphics().getHeight() - position.y
-								- image.getHeight());
-	}
-
-	protected abstract void setImage();
-
-	public Vector getPosition() {
-		return position.cpy();
-	}
+    
+    public Bitmap image;
+    
+    public Side side;
+    
+    public Game game;
+    
+    public SimpleObject(float x, float y, float width, float height) {
+    
+        super(x, y, width, height);
+    }
+    
+    public SimpleObject(Vector position, Bitmap image) {
+    
+        super(position.x, position.y, image.getWidth(), image.getHeight());
+        this.image = image;
+    }
+    
+    public SimpleObject(Side side, Game game) {
+    
+        this.side = side;
+        this.game = game;
+        setImage();
+        
+        float x =
+                (Side.ENEMY.equals(this.side)) ? this.game.getGraphics()
+                        .getWidth() - image.getWidth() - 1 : 0;
+        float y = 1;
+        
+        position = new Vector(x, y);
+        float w = image.getWidth();
+        float h = image.getHeight();
+        width = w;
+        height = h;
+        // bounds = new Rectangle(x - w / 2, y - h / 2, w, h);
+    }
+    
+    @Override
+    public void render() {
+    
+        game.getGraphics().drawBitmap(image, getRenderPosition(position));
+        Vector lowerLeft = getBounds().lowerLeft;
+        game.getGraphics().drawRect(getRenderPosition(lowerLeft), bounds.width,
+                bounds.height, Color.GREEN);
+    }
+    
+    protected abstract void setImage();
+    
+    public Vector getPosition() {
+    
+        return position.cpy();
+    }
+    
+    // превращает координаты модели в координаты холста
+    // вынести потом метод в общие утилиты
+    public Vector getRenderPosition(Vector v) {
+    
+        Vector pos = v.cpy();
+        pos.y = game.getGraphics().getHeight() - pos.y;
+        return pos.cpy();
+    }
 }
